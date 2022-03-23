9dd3fd594324361f110fc395d2def008fff27907
Christian Bromann <git@bromann.dev>
2022-03-23 22:26:41 +0100

    fix test on Mac

--- test/specs/basic.e2e.ts
+++ test/specs/basic.e2e.ts
@@ -36,6 +36,6 @@ describe('WDIO VSCode Service', () => {
     it('should be able to load VSCode', async () => {
         const workbench = await browser.getWorkbench()
         expect(await workbench.getTitleBar().getTitle())
-            .toBe('[Extension Development Host] - README.md - wdio-vscode-service - Visual Studio Code')
+            .toContain('[Extension Development Host] - README.md - wdio-vscode-service')
     })
 })
