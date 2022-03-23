ae9805bd6531ff6ee3f2a95c6e92db8ff0ee3c88
Christian Bromann <git@bromann.dev>
2022-03-23 23:40:00 +0100

    tweak

--- .github/scripts/link.mjs
+++ .github/scripts/link.mjs
@@ -4,13 +4,20 @@ import fs from 'fs/promises'
 import path from 'path'
 import url from 'url'
 
+console.log('Start linking wdio-vscode-service into node_modules');
+
 const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 const ROOT = path.join(__dirname, '..', '..')
 const LINKED_DIR = path.join(ROOT, 'node_modules', 'wdio-vscode-service')
 
+const relative = (from, to) => path.relative(
+    path.dirname(from),
+    path.resolve(to)
+)
+
 await fs.rm(LINKED_DIR, {
     recursive: true
 }).catch((e) => console.warn(`Linked dir doesn't exists: ${e.message}`))
-await fs.symlink('../', LINKED_DIR)
 
+await fs.symlink(relative(LINKED_DIR, ROOT), LINKED_DIR)
 console.log('Successful linked package');
