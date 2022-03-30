46228eefd22d80a441e75059eef720a7aee2c975
Christian Bromann <git@bromann.dev>
2022-03-30 15:11:40 +0200

    enable tests for activity bar

--- test/specs/basic.e2e.ts
+++ test/specs/basic.e2e.ts
@@ -93,49 +93,49 @@ describe('WDIO VSCode Service', () => {
         })
     })
 
-    // describe('activity bar', () => {
-    //     it('should show all activity bar items', async () => {
-    //         const workbench = await browser.getWorkbench()
-    //         const viewControls = await workbench.getActivityBar().getViewControls()
-    //         expect(await Promise.all(viewControls.map((vc) => vc.getTitle()))).toEqual([
-    //             'Explorer',
-    //             'Search',
-    //             'Source Control',
-    //             'Run and Debug',
-    //             'Extensions'
-    //         ])
-    //     })
+    describe('activity bar', () => {
+        it('should show all activity bar items', async () => {
+            const workbench = await browser.getWorkbench()
+            const viewControls = await workbench.getActivityBar().getViewControls()
+            expect(await Promise.all(viewControls.map((vc) => vc.getTitle()))).toEqual([
+                'Explorer',
+                'Search',
+                'Source Control',
+                'Run and Debug',
+                'Extensions'
+            ])
+        })
 
-    //     it('can open extension view and check that first installed extension is our guinea pig', async () => {
-    //         const workbench = await browser.getWorkbench()
-    //         const extensionView = await workbench.getActivityBar().getViewControl('Extensions')
-    //         await extensionView?.openView()
+        it('can open extension view and check that first installed extension is our guinea pig', async () => {
+            const workbench = await browser.getWorkbench()
+            const extensionView = await workbench.getActivityBar().getViewControl('Extensions')
+            await extensionView?.openView()
 
-    //         const selectedView = await workbench.getActivityBar().getSelectedViewAction()
-    //         expect(await selectedView.getTitle()).toBe('Extensions')
+            const selectedView = await workbench.getActivityBar().getSelectedViewAction()
+            expect(await selectedView.getTitle()).toBe('Extensions')
 
-    //         const sidebar = workbench.getSideBar()
-    //         const sidebarView = sidebar.getContent()
-    //         await sidebarView.getSection('EXTENSIONS')
+            const sidebar = workbench.getSideBar()
+            const sidebarView = sidebar.getContent()
+            await sidebarView.getSection('EXTENSIONS')
 
-    //         /**
-    //          * for some reason the developed extension doesn't show up
-    //          * in the installed extension section when running in a
-    //          * prestine environmnet
-    //          */
-    //         // const installedExtensions = await extensionViewSection.getVisibleItems()
-    //         // expect(await installedExtensions[0].getTitle()).toBe('Guinea Pig')
-    //     })
+            /**
+             * for some reason the developed extension doesn't show up
+             * in the installed extension section when running in a
+             * prestine environmnet
+             */
+            // const installedExtensions = await extensionViewSection.getVisibleItems()
+            // expect(await installedExtensions[0].getTitle()).toBe('Guinea Pig')
+        })
 
-    //     it('should be able to get global options', async () => {
-    //         const workbench = await browser.getWorkbench()
-    //         const viewControls = await workbench.getActivityBar().getGlobalActions()
-    //         expect(await Promise.all(viewControls.map((vc) => vc.getTitle()))).toEqual([
-    //             'Accounts',
-    //             'Manage'
-    //         ])
-    //     })
-    // })
+        it('should be able to get global options', async () => {
+            const workbench = await browser.getWorkbench()
+            const viewControls = await workbench.getActivityBar().getGlobalActions()
+            expect(await Promise.all(viewControls.map((vc) => vc.getTitle()))).toEqual([
+                'Accounts',
+                'Manage'
+            ])
+        })
+    })
 
     // describe('bottombar', () => {
     //     let bottomBar: BottomBarPanel
