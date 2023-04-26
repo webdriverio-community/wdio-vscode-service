befc3962ddde55c02406a75f2303ddab6100e551
Christian Bromann <git@bromann.dev>
2023-04-26 14:49:19 -0700

    remove yarn

--- example/test/example.e2e.ts
+++
@@ -1,9 +0,0 @@
-import { browser } from '@wdio/globals'
-
-describe('my webview', () => {
-    it('should be displayed', async () => {
-        console.log('Hello World!')
-        // eslint-disable-next-line wdio/no-pause
-        await browser.pause(5000)
-    })
-})
